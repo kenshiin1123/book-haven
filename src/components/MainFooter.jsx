c5a3b1e644e7672468ca2f6d12d53ddaffea7f2a
Lance Ivan Gil Fernandez <fernandezlanceivangil@gmail.com>
2025-06-05 13:24:10 +0800

    modify footer

--- src/components/MainFooter.jsx
+++ src/components/MainFooter.jsx
@@ -1,28 +1,82 @@
 import { Link } from "react-router";
 
+import { FaFacebook, FaLinkedin, FaGithubSquare } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
+import { TiHome } from "react-icons/ti";
+import { RiBookShelfLine } from "react-icons/ri";
 export default function MainFooter() {
   return (
-    <footer className="mt-auto bg-gray-200/50 w-full min-h-40 pb-2">
-      <div className="flex justify-around p-3 [&>section]:w-[30%] [&>section>h2]:text-center">
-        <section>
-          <h2 className="text-2xl font-semibold">Explore</h2>
-          <ul className="mt-3 font-medium">
-            <li>
-              <Link to={"/"}>Home</Link>
-            </li>
-            <li>
-              <Link to={"/books"}>Books</Link>
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h2 className="text-2xl font-semibold">Connect</h2>
-          {/* Add your social/contact links here */}
-        </section>
+    <footer className="mt-auto bg-gray-200/50 w-full h-max pb-2 border-t border-t-gray-600">
+      <div className="flex justify-around p-3  [&>section>h2]:text-center [&>section>h2]:flex [&>section>h2]:items-center">
+        <ExploreSection />
+        <ConnectSection />
       </div>
-      <small className="block text-center text-gray-600 mt-7">
+      <small className="block text-center text-gray-700">
         &copy;2025 Book Haven. All rights reserved.
       </small>
     </footer>
   );
 }
+
+const ExploreSection = () => {
+  return (
+    <section>
+      <h2 className="text-2xl font-semibold">Explore</h2>
+      <ul className="-translate-x-10 mt-2 font-medium [&>li>a]:hover:underline ">
+        <li>
+          <Link to={"/"} className="flex items-center gap-1">
+            <TiHome />
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to={"/books"} className="flex items-center gap-1">
+            <RiBookShelfLine />
+            Books
+          </Link>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+const ConnectSection = () => {
+  const links = [
+    {
+      icon: <FaFacebook />,
+      name: "Facebook",
+      href: "https://www.facebook.com/ivanbarrero23",
+    },
+    {
+      icon: <FaLinkedin />,
+      name: "LinkedIn",
+      href: "https://www.linkedin.com/in/lance-ivan-gil-fernandez-67bb02268/",
+    },
+    {
+      icon: <FaGithubSquare />,
+      name: "Github",
+      href: "https://github.com/kenshiin1123",
+    },
+    {
+      icon: <BiLogoGmail />,
+      name: "Gmail",
+      href: "https://mail.google.com/mail/u/0/?fs=1&to=fernandezlanceivangil@gmail.com&tf=cm",
+    },
+  ];
+
+  return (
+    <section>
+      <h2 className="text-2xl font-semibold">Connect</h2>
+      <ul className="-translate-x-10 mt-2 [&>li]:flex [&>li]:items-center [&>li]:gap-2 text-md font-medium space-y-1 [&>li>a]:hover:underline ">
+        {links.map((link) => (
+          <li key={link.name}>
+            {link.icon}
+            <a href={link.href} target="_blank" rel="noopener noreferrer">
+              {link.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};

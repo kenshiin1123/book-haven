e83938fb34bf7a16378b21bc0c94a93f99dfce56
Lance Ivan Gil Fernandez <128129648+kenshiin1123@users.noreply.github.com>
2025-07-10 15:31:03 +0800

    Add controller for book

--- server/routes/book.route.js
+++ server/routes/book.route.js
@@ -1,92 +1,29 @@
 import app from "express";
 const router = app.Router();
-import Book from "../model/book.model.js";
 
 import authMiddleware from "../middleware/authMiddleware.js";
 import validateBook from "../validations/validateBook.js";
 
-router.get("", async (_, res) => {
-  try {
-    const books = await Book.find();
-    if (!books) {
-      res.status(404).json({
-        message: "Failed to retrieve books",
-        success: false,
-        data: null,
-      });
-    }
+// Controller
+import {
+  fetchBooks,
+  patchBook,
+  postBook,
+  deleteBook,
+} from "../controller/book.controller.js";
 
-    res.status(200).json({
-      message: "Successfully retrieved books",
-      success: true,
-      data: books,
-    });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Could not retrieve book.", success: false });
-  }
-});
+router.get("", fetchBooks);
 
 // This is used to validate the token
 router.use(authMiddleware);
 
 // Create book
-router.post("", validateBook, async (req, res) => {
-  try {
-    const book = req.body;
-    const newBook = new Book({ ...book });
-    await newBook.save();
-    return res
-      .status(201)
-      .json({ message: "Book created", success: true, book: newBook });
-  } catch (error) {
-    res.status(500).json({ message: "Could not create book.", success: false });
-  }
-});
+router.post("", validateBook, postBook);
 
 // Update book
-router.patch("", validateBook, async (req, res) => {
-  try {
-    const book = req.body;
-    const updatedBook = await Book.findByIdAndUpdate(book._id, book);
-
-    if (!updatedBook) {
-      return res
-        .status(404)
-        .json({ message: "Book not found!", success: false });
-    }
-
-    return res
-      .status(201)
-      .json({ message: "Successfully Modified The Book!", success: true });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Could not modify the book.", success: false });
-  }
-});
+router.patch("", validateBook, patchBook);
 
 // Delete book
-router.delete("", validateBook, async (req, res) => {
-  try {
-    const book = req.body;
-    const deletedBook = await Book.findByIdAndDelete(book._id);
-
-    if (!deletedBook) {
-      return res
-        .status(404)
-        .json({ message: "Book not found!", success: false });
-    }
-
-    return res
-      .status(200)
-      .json({ message: "Successfully Deleted The Book!", success: true });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Could not delete the book.", success: false });
-  }
-});
+router.delete("", validateBook, deleteBook);
 
 export default router;
